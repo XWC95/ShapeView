34ed33c0bbf363ff263cbf5fc4881bfbcec60fff
xwc <m13052509803@163.com>
2018-03-04 16:58:45 +0800

    代码优化

---
+++ app/src/main/java/com/github/xwc/example/ExampleFragment.java
@@ -0,0 +1,29 @@
+package com.github.xwc.example;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+@SuppressLint("ValidFragment")
+public  class ExampleFragment extends Fragment {
+
+
+    private int layoutId;
+
+    public ExampleFragment(int layoutId) {
+        this.layoutId = layoutId;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(layoutId,container,false);
+    }
+
+}
--- app/src/main/java/com/github/xwc/example/MainActivity.java
+++ app/src/main/java/com/github/xwc/example/MainActivity.java
@@ -1,28 +1,51 @@
 package com.github.xwc.example;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Toast;
 
-import com.github.xwc.view.ShapeView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    ShapeView shapeView;
+//    ShapeView shapeView;
+
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    private  List<Fragment> list;
+    private MyAdapter adapter;
+    private String[] titles = {"demo1", "demo2","demo3"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.viewpager);
+        tabLayout = findViewById(R.id.tablayout);
 
-        shapeView = findViewById(R.id.heartShapeView);
-        shapeView.setClickListener(new com.github.xwc.view.View.ClickListener() {
-            @Override
-            public void onClick(View var1) {
-                Toast.makeText(MainActivity.this,"点击了",Toast.LENGTH_SHORT).show();
-            }
-        });
+        list = new ArrayList<>();
+        list.add(new ExampleFragment(R.layout.fragment_example1) );
+        list.add(new ExampleFragment(R.layout.fragment_example2));
+        list.add(new ExampleFragment(R.layout.fragment_example3));
+        adapter = new MyAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+//        shapeView = findViewById(R.id.heartShapeView);
+//        shapeView.setClickListener(new com.github.xwc.view.View.ClickListener() {
+//            @Override
+//            public void onClick(View var1) {
+//                Toast.makeText(MainActivity.this,"点击了",Toast.LENGTH_SHORT).show();
+//            }
+//        });
 //
 //
 //
@@ -35,5 +58,27 @@ public class MainActivity extends AppCompatActivity {
 
 //        shapeView.setHeartRadian(0.5f);
 //        shapeView.invalidate();
+    }
+
+
+    class MyAdapter extends FragmentPagerAdapter {
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
     }
 }
